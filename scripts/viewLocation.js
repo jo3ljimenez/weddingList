ba07533f102f10dd27ffccfc69e945e3555c0b98
Joel Jimenez <joel.jimenez@coppel.com>
2024-01-04 18:24:23 -0700

    Add location setcion

---
+++ scripts/viewLocation.js
@@ -0,0 +1,6 @@
+function openGoogleMaps() {
+    let coordinates  = "26.105220295758695, -108.75090080322276";
+    let googleMapsLink = "https://maps.google.com/?q=" + coordinates;
+
+    window.open(googleMapsLink, "_blank");
+}
